857aae9d6282eef5644f66231b1825a2146b204f
Jyotiraaditya <850066770@genpact.com>
2023-05-16 12:07:11 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Homepage';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
